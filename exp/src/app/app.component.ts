ef36e673a6060b9e959963ebbd51e1e75c0a3056
swebyte <vincent.karlsson94@gmail.com>
2023-12-28 11:16:42 +0100

    Add initial angular app

---
+++ exp/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'exp';
+
+  ngOnInit() 
+  {
+    console.log("init");
+  }
+}
