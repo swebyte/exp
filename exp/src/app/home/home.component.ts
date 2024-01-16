306be95195291fb56ecd451395fe56aba4dbea2c
swebyte <vincent.karlsson94@gmail.com>
2024-01-16 20:38:32 +0100

    Implement a home page with bootstrap and new font Fixes #5

--- exp/src/app/home/home.component.ts
+++ exp/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbRatingModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+	rating = 8;
 }
