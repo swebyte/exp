31e3f137f906ebec201ecd58515438904844c5b4
swebyte <vincent.karlsson94@gmail.com>
2024-01-22 20:33:18 +0100

    Add intersection observable animation directive on articles

---
+++ exp/src/app/directives/scrollanimationdirective.ts
@@ -0,0 +1,49 @@
+import { Directive, ElementRef, NgZone, Renderer2, Input, OnInit, OnDestroy } from '@angular/core';
+
+@Directive({
+  standalone: true,
+  selector: '[appScrollAnimation]'
+})
+export class ScrollAnimationDirective implements OnInit, OnDestroy {
+    @Input() threshold: number | number[] = [0.4, 0.8];
+
+  private observer: IntersectionObserver ;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.initIntersectionObserver();
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  private initIntersectionObserver() {
+    const options = {
+      threshold: this.threshold
+    };
+
+    const callback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(this.el.nativeElement, 'visible');
+        } else {
+          this.renderer.removeClass(this.el.nativeElement, 'visible');
+        }
+      });
+    };
+
+    // Use NgZone.runOutsideAngular to avoid change detection during scrolling
+    this.ngZone.runOutsideAngular(() => {
+      this.observer = new IntersectionObserver(callback, options);
+      this.observer.observe(this.el.nativeElement);
+    });
+  }
+}
--- exp/src/app/home/home.component.ts
+++ exp/src/app/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScrollAnimationDirective } from '../directives/scrollanimationdirective';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgbRatingModule],
+  imports: [NgbRatingModule, ScrollAnimationDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 	rating = 8;
+
+  
+  ngOnInit()
+  {
+
+  }
+
 }
