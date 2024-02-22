6f5c7d7eddb8ee83ecf46cb38970fadba70259ab
swebyte <vincent.karlsson94@gmail.com>
2024-02-22 20:30:53 +0100

    WIP 3d behind content body

--- exp/src/app/about-me/about-me.component.ts
+++ exp/src/app/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from '../skills/skills.component';
+import { CanvasBoxComponent } from '../3d/canvas-box/canvas-box.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SkillsComponent],
+  imports: [SkillsComponent, CanvasBoxComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
