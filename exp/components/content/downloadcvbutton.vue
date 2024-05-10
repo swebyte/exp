5814ccde93d443dda356649146ccfb9f49894877
vincent.karlsson <vincent.karlsson94@gmail.com>
2024-05-10 16:06:42 +0200

    Fix CV download link not working in production

--- exp/components/content/downloadcvbutton.vue
+++ exp/components/content/downloadcvbutton.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="m-0">
-            <a href="/Karlsson_Vincent_Eng.pdf" download="" 
-            class="block h-full  bg-color1 hover:bg-crimson-light
+            <a :href="`${$config.app.baseURL}Karlsson_Vincent_Eng.pdf`" download="" 
+            class="block h-full bg-color1 hover:bg-crimson-light
              text-color4 py-1 px-3 text-center flex items-center justify-center">
              <Icon name="material-symbols:download" class="mr-1"></Icon>Download CV 
             </a>
