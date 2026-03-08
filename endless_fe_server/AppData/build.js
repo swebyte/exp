3340fad75d0e96fd1cfbf394e0feb54f22fd5595
vincent.karlsson94@gmail.com <vincent.karlsson94@gmail.com>
2026-03-08 22:35:03 +0100

    Update assets

--- endless_fe_server/AppData/build.js
+++ endless_fe_server/AppData/build.js
@@ -111,13 +111,12 @@ var __decorateClass = (decorators, target, key, kind) => {
 
 let HelloWorld = class extends rogue_engine__WEBPACK_IMPORTED_MODULE_0__.Component {
   awake() {
-    console.log("Hello, Rogue Engine!");
+    console.log("Hello, from HelloWorld:awake()!");
   }
   start() {
-    console.log("Hello, Rogue Engine!");
+    console.log("Hello, from HelloWorld:start()!");
   }
   update() {
-    console.log("Hello, Rogue Engine!");
   }
 };
 __name(HelloWorld, "HelloWorld");
@@ -440,6 +439,88 @@ __name(TerrainRepeat, "TerrainRepeat");
 TerrainRepeat = __decorateClass([
   rogue_engine__WEBPACK_IMPORTED_MODULE_0__.registerComponent
 ], TerrainRepeat);
+
+
+
+/***/ }),
+
+/***/ "./Assets/Scripts/WaterWaves.re.ts":
+/*!*****************************************!*\
+  !*** ./Assets/Scripts/WaterWaves.re.ts ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ WaterWaves)
+/* harmony export */ });
+/* harmony import */ var rogue_engine__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! rogue-engine */ "rogue-engine");
+/* harmony import */ var rogue_engine__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(rogue_engine__WEBPACK_IMPORTED_MODULE_0__);
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+var __decorateClass = (decorators, target, key, kind) => {
+  var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+  for (var i = decorators.length - 1, decorator; i >= 0; i--)
+    if (decorator = decorators[i])
+      result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+  if (kind && result)
+    __defProp(target, key, result);
+  return result;
+};
+
+let WaterWaves = class extends rogue_engine__WEBPACK_IMPORTED_MODULE_0__.Component {
+  constructor() {
+    super(...arguments);
+    this.waveHeight = 5;
+    this.waveSpeed = 0.5;
+    this.waveFrequency = 0.02;
+    this.time = 0;
+  }
+  start() {
+    const mesh = this.object3d;
+    if (!mesh.geometry)
+      return;
+    mesh.geometry = mesh.geometry.toNonIndexed();
+    this.positions = mesh.geometry.attributes.position;
+    this.baseX = new Float32Array(this.positions.count);
+    this.baseY = new Float32Array(this.positions.count);
+    this.baseZ = new Float32Array(this.positions.count);
+    for (let i = 0; i < this.positions.count; i++) {
+      this.baseX[i] = this.positions.getX(i);
+      this.baseY[i] = this.positions.getY(i);
+      this.baseZ[i] = this.positions.getZ(i);
+    }
+  }
+  update() {
+    if (!this.positions)
+      return;
+    this.time += rogue_engine__WEBPACK_IMPORTED_MODULE_0__.Runtime.deltaTime * this.waveSpeed;
+    const f = this.waveFrequency;
+    const h = this.waveHeight;
+    for (let i = 0; i < this.positions.count; i++) {
+      const x = this.baseX[i];
+      const y = this.baseY[i];
+      const z = this.baseZ[i] + Math.sin(x * f + this.time) * h * 0.5 + Math.cos(y * f + this.time) * h * 0.5;
+      this.positions.setZ(i, z);
+    }
+    this.positions.needsUpdate = true;
+    this.object3d.geometry.computeVertexNormals();
+  }
+};
+__name(WaterWaves, "WaterWaves");
+__decorateClass([
+  rogue_engine__WEBPACK_IMPORTED_MODULE_0__.props.num()
+], WaterWaves.prototype, "waveHeight", 2);
+__decorateClass([
+  rogue_engine__WEBPACK_IMPORTED_MODULE_0__.props.num()
+], WaterWaves.prototype, "waveSpeed", 2);
+__decorateClass([
+  rogue_engine__WEBPACK_IMPORTED_MODULE_0__.props.num()
+], WaterWaves.prototype, "waveFrequency", 2);
+WaterWaves = __decorateClass([
+  rogue_engine__WEBPACK_IMPORTED_MODULE_0__.registerComponent
+], WaterWaves);
 
 
 
@@ -13875,7 +13956,8 @@ let A;const I=new Array(128).fill(void 0);function g(A){return I[A]}I.push(void 
 /******/ 	__webpack_require__("./Assets/Scripts/HelloWorld.re.ts");
 /******/ 	__webpack_require__("./Assets/Scripts/NetworkManager.re.ts");
 /******/ 	__webpack_require__("./Assets/Scripts/PlayerController.re.ts");
-/******/ 	var __webpack_exports__ = __webpack_require__("./Assets/Scripts/TerrainRepeat.re.ts");
+/******/ 	__webpack_require__("./Assets/Scripts/TerrainRepeat.re.ts");
+/******/ 	var __webpack_exports__ = __webpack_require__("./Assets/Scripts/WaterWaves.re.ts");
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ 	return __webpack_exports__;
@@ -13883,4 +13965,4 @@ let A;const I=new Array(128).fill(void 0);function g(A){return I[A]}I.push(void 
 ;
 });
 //# sourceMappingURL=rogue-engine-user-scripts.js.map
-window['rogue-engine'].App.play({"title":"Endless","scenes":[{"name":"world1","uuid":"31eebf83-ae58-47ce-bfa0-e0cd08a91387"}],"projectVersion":30,"assetPaths":{"054b79e9-3c1c-46aa-a813-36d78dfa7217":"AppData/054b79e9-3c1c-46aa-a813-36d78dfa7217.roguePrefab","b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2":"AppData/b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2.roguePrefab","b7152b85-7f32-44fc-ac63-e3aff6241d28":"AppData/b7152b85-7f32-44fc-ac63-e3aff6241d28.roguePrefab","35bc250c-2f12-4bd9-b813-f149d1045ff9":"AppData/35bc250c-2f12-4bd9-b813-f149d1045ff9.rogueAnimation","5314d7bb-9f7f-4484-a387-028d7ebb16b6":"AppData/5314d7bb-9f7f-4484-a387-028d7ebb16b6.rogueAnimation","6aa0fe1f-ad2e-43ee-94c2-61fbb266364c":"AppData/6aa0fe1f-ad2e-43ee-94c2-61fbb266364c.rogueAnimation","4e2644b9-8be8-4b37-9cd4-5bc01760d5fb":"AppData/4e2644b9-8be8-4b37-9cd4-5bc01760d5fb.rogueAnimation","c97e0e1a-8f95-49c2-bc23-1e064c5e50e2":"AppData/c97e0e1a-8f95-49c2-bc23-1e064c5e50e2.rogueAnimation","ce700725-33ed-4cdf-b98c-4787f69a2223":"AppData/ce700725-33ed-4cdf-b98c-4787f69a2223.rogueAnimation","1fae0ca2-3e38-431b-8475-ae5b9836710c":"AppData/1fae0ca2-3e38-431b-8475-ae5b9836710c.png","d7c0ad5d-9fb0-4304-8b6d-789cc25b0e79":"AppData/d7c0ad5d-9fb0-4304-8b6d-789cc25b0e79.png","ef8212ed-e4ae-491f-a433-df009b34db52":"AppData/ef8212ed-e4ae-491f-a433-df009b34db52.png","f08b52c4-ca2a-4f50-be8f-e00b09e5e4df":"AppData/f08b52c4-ca2a-4f50-be8f-e00b09e5e4df.rogueMaterial","b70d1958-96c0-43f1-92e8-47e0e6deca62":"AppData/b70d1958-96c0-43f1-92e8-47e0e6deca62.roguePrefab","5c9966c2-951b-426a-8819-c70ca51579b5":"AppData/5c9966c2-951b-426a-8819-c70ca51579b5.roguePrefab","87500c41-20e9-4cf7-84d8-0ed4a80d3ff7":"AppData/87500c41-20e9-4cf7-84d8-0ed4a80d3ff7.roguePrefab","38f9a99e-e4a4-47a1-b44c-3205d637854f":"AppData/38f9a99e-e4a4-47a1-b44c-3205d637854f.roguePrefab","32cf395b-c12f-43e8-a902-5aa7a54b3e9d":"AppData/32cf395b-c12f-43e8-a902-5aa7a54b3e9d.roguePrefab","438905ce-db74-483e-8609-4d1371ea1a15":"AppData/438905ce-db74-483e-8609-4d1371ea1a15.png","353fd7ab-4612-4a05-b7ea-8cf2b585152d":"AppData/353fd7ab-4612-4a05-b7ea-8cf2b585152d.png","f57be6d7-9031-453c-a06b-2e7bb3fbda9d":"AppData/f57be6d7-9031-453c-a06b-2e7bb3fbda9d.png","0233316f-3b99-43b2-952c-d7b73e23b09a":"AppData/0233316f-3b99-43b2-952c-d7b73e23b09a.png","c3818102-6945-4558-bbcc-fd27e915ac8b":"AppData/c3818102-6945-4558-bbcc-fd27e915ac8b.png","5aebac88-a745-406e-8f97-9c01b8c0e033":"AppData/5aebac88-a745-406e-8f97-9c01b8c0e033.png","958e20b8-e5ea-43f9-a3d9-cad3bc88db74":"AppData/958e20b8-e5ea-43f9-a3d9-cad3bc88db74.jpg","84c92300-c579-49b8-a8db-714c5512aedf":"AppData/84c92300-c579-49b8-a8db-714c5512aedf.png","78637a2b-1355-4e69-b2af-39dbacb5aa64":"AppData/78637a2b-1355-4e69-b2af-39dbacb5aa64.jpg","f3f69665-f1b6-465d-b946-b6243ea24066":"AppData/f3f69665-f1b6-465d-b946-b6243ea24066.rogueMaterial","31eebf83-ae58-47ce-bfa0-e0cd08a91387":"AppData/31eebf83-ae58-47ce-bfa0-e0cd08a91387.rogueScene"},"namedPrefabUUIDs":{"Rock_3_O_Color1":"054b79e9-3c1c-46aa-a813-36d78dfa7217","Rock_3_P_Color1":"b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2","Rock_3_Q_Color1":"b7152b85-7f32-44fc-ac63-e3aff6241d28","ThirdPersonCharacter":"32cf395b-c12f-43e8-a902-5aa7a54b3e9d","Tree_3_C_Color1":"5c9966c2-951b-426a-8819-c70ca51579b5","ExampleCar":"87500c41-20e9-4cf7-84d8-0ed4a80d3ff7","FirstPersonCharacter":"38f9a99e-e4a4-47a1-b44c-3205d637854f"}});
+window['rogue-engine'].App.play({"title":"Endless","scenes":[{"name":"world1","uuid":"31eebf83-ae58-47ce-bfa0-e0cd08a91387"}],"projectVersion":30,"assetPaths":{"054b79e9-3c1c-46aa-a813-36d78dfa7217":"AppData/054b79e9-3c1c-46aa-a813-36d78dfa7217.roguePrefab","b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2":"AppData/b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2.roguePrefab","b7152b85-7f32-44fc-ac63-e3aff6241d28":"AppData/b7152b85-7f32-44fc-ac63-e3aff6241d28.roguePrefab","35bc250c-2f12-4bd9-b813-f149d1045ff9":"AppData/35bc250c-2f12-4bd9-b813-f149d1045ff9.rogueAnimation","5314d7bb-9f7f-4484-a387-028d7ebb16b6":"AppData/5314d7bb-9f7f-4484-a387-028d7ebb16b6.rogueAnimation","6aa0fe1f-ad2e-43ee-94c2-61fbb266364c":"AppData/6aa0fe1f-ad2e-43ee-94c2-61fbb266364c.rogueAnimation","4e2644b9-8be8-4b37-9cd4-5bc01760d5fb":"AppData/4e2644b9-8be8-4b37-9cd4-5bc01760d5fb.rogueAnimation","c97e0e1a-8f95-49c2-bc23-1e064c5e50e2":"AppData/c97e0e1a-8f95-49c2-bc23-1e064c5e50e2.rogueAnimation","ce700725-33ed-4cdf-b98c-4787f69a2223":"AppData/ce700725-33ed-4cdf-b98c-4787f69a2223.rogueAnimation","d08248a2-ecf7-48b5-bd0d-aee82e84364a":"AppData/d08248a2-ecf7-48b5-bd0d-aee82e84364a.png","f68b1c3e-2288-4bda-bdd0-f4db8e3f05c6":"AppData/f68b1c3e-2288-4bda-bdd0-f4db8e3f05c6.jpg","f08b52c4-ca2a-4f50-be8f-e00b09e5e4df":"AppData/f08b52c4-ca2a-4f50-be8f-e00b09e5e4df.rogueMaterial","b70d1958-96c0-43f1-92e8-47e0e6deca62":"AppData/b70d1958-96c0-43f1-92e8-47e0e6deca62.roguePrefab","5c9966c2-951b-426a-8819-c70ca51579b5":"AppData/5c9966c2-951b-426a-8819-c70ca51579b5.roguePrefab","87500c41-20e9-4cf7-84d8-0ed4a80d3ff7":"AppData/87500c41-20e9-4cf7-84d8-0ed4a80d3ff7.roguePrefab","38f9a99e-e4a4-47a1-b44c-3205d637854f":"AppData/38f9a99e-e4a4-47a1-b44c-3205d637854f.roguePrefab","32cf395b-c12f-43e8-a902-5aa7a54b3e9d":"AppData/32cf395b-c12f-43e8-a902-5aa7a54b3e9d.roguePrefab","438905ce-db74-483e-8609-4d1371ea1a15":"AppData/438905ce-db74-483e-8609-4d1371ea1a15.png","353fd7ab-4612-4a05-b7ea-8cf2b585152d":"AppData/353fd7ab-4612-4a05-b7ea-8cf2b585152d.png","f57be6d7-9031-453c-a06b-2e7bb3fbda9d":"AppData/f57be6d7-9031-453c-a06b-2e7bb3fbda9d.png","0233316f-3b99-43b2-952c-d7b73e23b09a":"AppData/0233316f-3b99-43b2-952c-d7b73e23b09a.png","c3818102-6945-4558-bbcc-fd27e915ac8b":"AppData/c3818102-6945-4558-bbcc-fd27e915ac8b.png","5aebac88-a745-406e-8f97-9c01b8c0e033":"AppData/5aebac88-a745-406e-8f97-9c01b8c0e033.png","8784bcee-98d5-4c60-9ed1-4d41c3622e40":"AppData/8784bcee-98d5-4c60-9ed1-4d41c3622e40.jpg","84c92300-c579-49b8-a8db-714c5512aedf":"AppData/84c92300-c579-49b8-a8db-714c5512aedf.png","78637a2b-1355-4e69-b2af-39dbacb5aa64":"AppData/78637a2b-1355-4e69-b2af-39dbacb5aa64.jpg","f3f69665-f1b6-465d-b946-b6243ea24066":"AppData/f3f69665-f1b6-465d-b946-b6243ea24066.rogueMaterial","60b1d34f-18bc-4fd2-b13e-9ce4ece547c0":"AppData/60b1d34f-18bc-4fd2-b13e-9ce4ece547c0.png","cb57b00a-c67f-467b-b44b-41cbee50fac7":"AppData/cb57b00a-c67f-467b-b44b-41cbee50fac7.rogueMaterial","4f94874e-0191-4f82-9f78-df9cc3b25d7f":"AppData/4f94874e-0191-4f82-9f78-df9cc3b25d7f.rogueMaterial","31eebf83-ae58-47ce-bfa0-e0cd08a91387":"AppData/31eebf83-ae58-47ce-bfa0-e0cd08a91387.rogueScene"},"namedPrefabUUIDs":{"Rock_3_O_Color1":"054b79e9-3c1c-46aa-a813-36d78dfa7217","Rock_3_P_Color1":"b1dd03d1-30d6-4824-ad0d-eb62c88d8aa2","Rock_3_Q_Color1":"b7152b85-7f32-44fc-ac63-e3aff6241d28","ThirdPersonCharacter":"32cf395b-c12f-43e8-a902-5aa7a54b3e9d","Tree_3_C_Color1":"5c9966c2-951b-426a-8819-c70ca51579b5","ExampleCar":"87500c41-20e9-4cf7-84d8-0ed4a80d3ff7","FirstPersonCharacter":"38f9a99e-e4a4-47a1-b44c-3205d637854f"}});
